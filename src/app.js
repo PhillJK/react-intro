bf9a047293c00886068339ffc35658cc57a52dfb
PhillJK <farrux2315@gmail.com>
2020-09-23 16:44:00 +0500

    Added lazy load to app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import { render } from "react-dom";
 import { Router } from "@reach/router";
 import SearchParams from "./SearchParams";
-import Details from "./Details";
 import ThemeContext from "./ThemeContext";
 import Navbar from "./Navbar";
+
+const Details = lazy(() => import("./Details"));
 
 const App = () => {
   const themeHook = useState("crimson");
@@ -12,10 +13,12 @@ const App = () => {
     <ThemeContext.Provider value={themeHook}>
       <div>
         <Navbar />
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
+        <Suspense fallback={<h1>loading route ...</h1>}>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
